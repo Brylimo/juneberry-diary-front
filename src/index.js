342fb413ce43c2d339a0bcb84d108847c45fca08
brylimo <icj0103@gmail.com>
2024-01-01 23:55:37 +0900

    cookie 세팅 작업중

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
+import { CookieProvider } from 'react-cookie';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import createSagaMiddleware from 'redux-saga';
@@ -21,9 +22,11 @@ sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <CookieProvider>
     <Provider store={store}>
       <App />
     </Provider>
+  </CookieProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
