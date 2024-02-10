8e574814e97934288b3b99c878c2b70ee18d55f4
brylimo <icj0103@gmail.com>
2024-02-10 16:59:41 +0900

    ec2 deploy connection test

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: "http://localhost:8081/api",
+    baseURL: "http://54.162.224.247:80/api",
     headers: {
         "Content-Type": "application/json",
     },
