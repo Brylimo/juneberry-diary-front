2ea045a16c7270b9ed6eff379ae2b3b76f4852fb
brylimo <icj0103@gmail.com>
2024-02-10 18:14:38 +0900

    baseurl 변경

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: "http://54.162.224.247:80/api",
+    baseURL: "http://54.224.127.97:80/api",
     headers: {
         "Content-Type": "application/json",
     },
