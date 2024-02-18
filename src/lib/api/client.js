551359374a159cafe97756999edb37513f50d940
brylimo <icj0103@gmail.com>
2024-02-18 20:04:22 +0900

    .env 파일 생성하여 baseurl 변경

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: "https://juneberrydiary.store/api",
+    baseURL: process.env.REACT_APP_BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
