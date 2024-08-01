79ec389c4a3aecd3f0d094eadd9257b937db1272
brylimo <icj0103@gmail.com>
2024-08-01 10:56:21 +0900

    profile card layout 개발

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -2,17 +2,37 @@ import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import ProfileCard from './ProfileCard';
 
-const ProfileContent = styled.div`
+const ProfileBlock = styled.div`
     position: absolute;
     top: 8rem;
     min-height: calc(100vh - 8rem);
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+`;
+
+const ProfileLeftSidebar = styled.div`
+    width: 280px;
+    display: flex;
+    justify-content: center;
+    margin-top: 3rem;
+    margin-left: 2rem;
+    margin-right: 2rem;
+`;
+
+const ProfileContent = styled.div`
+    flex: 6;
 `
 
 const Profile = () => {
     return (
-        <ProfileContent>
-            <ProfileCard />
-        </ProfileContent>
+        <ProfileBlock>
+            <ProfileLeftSidebar>
+                <ProfileCard />
+            </ProfileLeftSidebar>
+            <ProfileContent>
+            </ProfileContent>
+        </ProfileBlock>
     )
 }
 
--- src/components/user/ProfileCard.jsx
+++ src/components/user/ProfileCard.jsx
@@ -1,9 +1,54 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import styled from 'styled-components';
 
+const ProfileCardBlock = styled.div`
+    width: 100%;
+    border-radius: 1rem;
+    border: 1px solid #d0d7de;
+    height: 60rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
+`
+
+const ProfileCardImgBlock = styled.div`
+    position: relative;
+    width: 100%;
+    background-color: black;
+    border-radius: 50%;
+
+    &:after {
+        content: "";
+        display: block;
+        padding-bottom: 100%;
+    }
+`;
+
+const ProfileCardInfoBlock = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin-top: 1.5rem;
+`;
+
+const PCInfoUserName = styled.div`
+    font-weight: bold;
+    font-size: 35px;
+`;
+
 const ProfileCard = () => {
+
     return (
-        <div>hi</div>
+        <ProfileCardBlock>
+            <ProfileCardImgBlock>
+            </ProfileCardImgBlock>
+            <ProfileCardInfoBlock>
+                <PCInfoUserName>
+                    brylimo
+                </PCInfoUserName>
+            </ProfileCardInfoBlock>
+        </ProfileCardBlock>
     )
 }
 
